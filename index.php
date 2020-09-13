9695b100f154c3281cf7b43eac35734b0a57bfdf
yossy0201 <yossy0201msk@gmail.com>
2020-09-14 00:13:28 +0900

    introduction

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 
 $name = "イチロー";
 
-echo  $name
+echo  $name;
+
+echo "<p>${name}はマリナーズで活躍しています。</p>";
+
+$number = "51";
+
+echo "<p>背番号 : ${number}</p>";
+
+$food = "カレーライス";
+
+echo "<p>彼は${food}が好きです";
 
 
 
